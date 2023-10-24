eeaa8a4f5a5eb3334e5a5aaf6c0d2b004d44a6d2
YasminGon <yasmingon08@hotmail.com>
2023-10-24 15:00:33 +0000

    salvando

--- Carro.java
+++ Carro.java
@@ -1,9 +1,41 @@
 public class Carro{
     private String cor;
     private String modelo;
+    private String placa;
     private int aro;
     private double valorCarro;
-
+    
+public int getAro(){
+    return aro;
+}
+public void setAro(int aro){
+    this.aro = aro;
+}
+public String getCor(){
+    return cor;
+}
+public void setCor(String cor){
+    this.cor = cor;
+}
+public String getModelo(){
+    return modelo;
+}
+public void setModelo(String modelo){
+    this.modelo = modelo;
+}
+public String getPlaca(){
+    return placa;
+}
+public void setPlaca(String placa){
+    this.placa = placa;
+}
+public double getValorCarro(){
+    return valorCarro;
+}
+public void setAro(double valorCarro){
+    this.valorCarro = valorCarro
+    ;
+}
     public Carro(){
 
     }
@@ -18,6 +50,19 @@ public class Carro{
         this.aro=aro;
         this.modelo=modelo;
         this.valorCarro=valorCarro;
+    }
+    public Carro(int aro, String modelo, double valorCarro, String cor){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valorCarro=valorCarro;
+        this.cor=cor;
+    }
+    public Carro(int aro, String modelo, double valorCarro, String cor, String placa){
+        this.aro=aro;
+        this.modelo=modelo;
+        this.valorCarro=valorCarro;
+        this.cor=cor;
+        this.placa=placa;
     }
     //4 atributos para o Carro e faça três construtores
 }
--- Principal.java
+++ Principal.java
@@ -1,6 +1,14 @@
 public class Principal {
-    public static void main(String args){
+    public static void main(String[] args){
 
-        
-    }
+        Carro car = new Carro();
+        Carro carAro = new Carro(18);
+        Carro carDoisConstrutores = new Carro(18, "dodge Ram 2023", 500.000, "Azul", "JYG 2808");
+        Carro carQuatroParametros = new Carro(18, "dodge Ram 2023", 500.000, "Preta", "MIN 2808");
+
+        System.out.println(carAro.getAro());
+        System.out.println(carQuatroParametros);
+        System.out.println(carAro);
+        System.out.println(carAro);
+       }
 }
